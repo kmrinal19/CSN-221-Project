abbba678c4e657c5e0f95750a036ba5d7a524956
Anupriya-Dey <96530004+Anupriya-Dey@users.noreply.github.com>
2022-10-18 14:35:31 +0530

    Combined execution stage to a single file.
    Testbench working succesfull for rtype and addi

--- execution/ex_tb.v
+++ execution/ex_tb.v
@@ -1,22 +1,22 @@
 `timescale 1ns / 1ps
-+define+FILE="EX.v"
-`include `FILE
-module example_tb();
+`include "execution/EX.v"
+module testb;
     reg clk;
     reg reset;
     integer rs;
     integer rt;
-    reg [31:0] sign_ext;
-    reg [31:0] pc;
+    integer sign_ext;
+    integer pc;
     reg ALUSrc;
     reg [1:0] ALUOp;
     reg [5:0] funct;
+  	reg branch;
     wire [31:0] address;
     wire zero;
     wire [31:0] resultOut;
     wire [31:0] pcout;
 
-    EX tb (
+    main tb (
         .clk (clk),
         .reset (reset),
         .rs (rs),
@@ -29,7 +29,8 @@ module example_tb();
         .address(address),
         .zero (zero),
         .resultOut(resultOut),
-        .pcout (pcout)
+        .pcout (pcout),
+      	.branch (branch)
     );
 
     initial 
@@ -41,20 +42,25 @@ module example_tb();
     initial 
     begin
         reset = 1'b1;
-        #10 reset = 1'b0;
+        #100 reset = 1'b0;
     end
 
     initial 
     begin
-        $display ("time=%3d, address=%b, zero=%b, result=%d, pcout=%b \n", $time, address, zero, resultOut, pcout);
+        
+        
         #20
-        rs = 32'd5;
-        rt = 32'd3;
-        pc = 32'd4;
-        sign_ext = 32'd6;
-        ALUSrc = 1'b0;
-        funct = 6'b000000;
+        rs = 5;
+        rt = 5;
+        pc = 4;
+        sign_ext = 5;
+        ALUSrc = 1'b1;
+        funct = 6'b000010;
         ALUOp = 2'b10;
+      	branch = 1'b0;
+        #40
+        $display ("time=%3d, address=%b, zero=%b, result=%d, pcout=%b \n", $time, address, zero, resultOut, pcout);
+      	$finish;
 
     end
 
